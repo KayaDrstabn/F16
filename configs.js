b3e8a2197adc2c9b5915462ba88f98ff4d8a18ad
KayaDrstabn <108328937+KayaDrstabn@users.noreply.github.com>
2024-05-15 21:36:24 +0400

    Add files via upload

---
+++ configs.js
@@ -0,0 +1,14 @@
+module.exports = {
+  token: "", //Bot Token
+  port: "80",
+  prefix: "", //Bot Prefix
+  client_id: "", //Client ID
+  client_secret: "", //Client Secret
+  redirect_uri: "https://123456789.replit.dev:80", //Changing the Port Part!
+  idserver: "", //Server ID Main
+  idrole: "", //Role ID Main
+  webhook: "", //Auth Log
+  webhookBackup: "", //wehbook de backup do object.json
+  authLink: ``, //Auth Link
+  owners: ["1049023653536337920", "", ""], //Owner ID
+};
